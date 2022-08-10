6e8080da7504f37b4dd9f780b0c9b736a290cde9
Aropero-PFM <59558013+Aropero-PFM@users.noreply.github.com>
2022-08-10 14:04:22 +0200

    Se modifica el .gitignore para que contemple las librerias

--- include/version.h
+++ include/version.h
@@ -1,12 +1,12 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "211"
+  #define BUILD_NUMBER "213"
 #endif
 #ifndef VERSION
-  #define VERSION "v1.0211 - 2022-08-10 13:04:19.077481"
+  #define VERSION "v1.0213 - 2022-08-10 14:03:37.474063"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v1.0211"
+  #define VERSION_SHORT "v1.0213"
 #endif
 #ifndef RELEASE_VERSION_SUB_VERSION
   #define RELEASE_VERSION_SUB_VERSION "1.0"
